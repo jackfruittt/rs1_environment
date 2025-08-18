5c6d8a874db1d13d9fbd5c44e3931f77d270153a
ace-rv <aceviray5@gmail.com>
2025-08-18 20:21:57 +1000

    Made sample environment based off 41068_bringup package

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rs1_environment
 Repo for the RS1 environment
+
+# How to launch the environment
+# This launches a pre-defined environment created through gazebo
+ros2 launch rs1_environment spawn_environment.launch.py 
+
+# The models used in the environment uses a mix of Fuel Gazebo models, online 3D models and custom ones
---
+++ launch/spawn_environment.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.substitutions import (Command, LaunchConfiguration,
+                                  PathJoinSubstitution)
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    # Get paths to directories
+    pkg_path = FindPackageShare('rs1_environment')
+    config_path = PathJoinSubstitution([pkg_path,
+                                       'config'])
+
+    # Start Gazebo to simulate the robot in the chosen world
+    world_launch_arg = DeclareLaunchArgument(
+        'world',
+        default_value='mountain_forest',
+        description='Which world to load',
+        choices=['mountain_forest'] # Add more worlds here , seperated by commas
+    )
+    ld.add_action(world_launch_arg)
+
+    gazebo = IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('ros_ign_gazebo'),
+                             'launch', 'ign_gazebo.launch.py']),
+        launch_arguments={
+            'ign_args': [PathJoinSubstitution([pkg_path,
+                                               'worlds',
+                                               [LaunchConfiguration('world'), '.sdf']]),
+                         ' -r']}.items()
+    )
+    ld.add_action(gazebo)
+
+    ## TO-DO Add the robot model to the simulation stuff here and gazebo bridge stuff 
+
+    ##
+
+
+    return ld
